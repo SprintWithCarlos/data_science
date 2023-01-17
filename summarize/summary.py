ca59593cd9bcd2e5b303e12b9417a62d0f03e284
Carlos Marchena <sprintwithcarlos@pm.me>
2023-01-17 10:40:53 +0100

    add summarize

---
+++ summarize/summary.py
@@ -0,0 +1,84 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import os
+import numpy as np
+import openai
+from pytube import YouTube
+
+
+def summarize(url, api_key, language="en"):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    filename = os.path.join(current_dir, "summary.md")
+    language_array = [language]
+    os.environ["OPENAI_API_KEY"] = api_key
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    yt = YouTube(url)
+    title = yt.title
+    author = yt.author
+    video_id = url.split("=")[1]
+    try:
+        transcription = YouTubeTranscriptApi.get_transcript(
+            video_id, language_array)
+        data = [f'Transcript of "{title.lower()}" by {author}:']
+        for item in transcription:
+            data.append(item['text'].lower())
+        max_chunk_length = 2048
+        chunks = []
+        current_chunk = []
+        current_chunk_length = 0
+        for sentence in data:
+            current_chunk_length += len(sentence)
+            if current_chunk_length > max_chunk_length:
+                chunks.append(current_chunk)
+                current_chunk = []
+                current_chunk_length = 0
+            current_chunk.append(sentence)
+        chunks.append(current_chunk)
+        summary_responses = []
+        for chunk in chunks:
+            sentences = " ".join(list(chunk))
+            prompt = f"{sentences}\n\nTl;dr"
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=prompt,
+                temperature=0.7,
+                max_tokens=300,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=1
+            )
+            response_text = response["choices"][0]["text"]
+            summary_responses.append(response_text)
+        sentence = " ".join(summary_responses).replace(
+            ": ", "").replace("\n", "")
+        prompt = f"{sentence}\n\nTl;dr"
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=300,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=1
+        )
+        executive_summary = response["choices"][0]["text"].replace(": ", "")
+        new_array = [
+            f"""# Transcript of "{title.capitalize()}" by {author.capitalize()}
+Link: [{url}]({url})
+## Executive Summary:
+{executive_summary}
+## Main Takeaways:
+  """]
+        for i, item in enumerate(summary_responses):
+            new_item = item.replace(": ", "").replace("\n", "")
+            new_array.append(f"""{i + 1}. {new_item}\n  """)
+        sentence = "".join(new_array)
+        with open(filename, "w") as f:
+            f.write(sentence)
+    except Exception as e:
+        lines = str(e).splitlines()
+        lines_array = []
+        for i in range(12):
+            lines_array.append(lines[i])
+        message = """ """.join(list(lines_array))
+        return (f"""An error occurred: {message}""")
---
+++ summarize/test_summary.py
@@ -0,0 +1,40 @@
+import pytest
+import os
+from mock import patch
+from summary import summarize
+import time
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(current_dir, "summary.md")
+
+
+@patch('summary.YouTubeTranscriptApi.get_transcript', return_value=[{'text': 'This is a test transcript.'}])
+@patch('openai.Completion.create', return_value={'choices': [{'text': 'This is a summary.'}]})
+@patch('summary.YouTube')
+def test_summarize(mock_youtube, mock_openai_create, mock_yt_transcripts):
+    if os.path.exists(filename):
+        os.remove(filename)
+        print(f"{filename} deleted!")
+    # time.sleep(5)
+    # delete_file is being accessed here
+
+    mock_youtube_instance = mock_youtube.return_value
+    mock_youtube_instance.title = 'Test Title'
+    mock_youtube_instance.author = 'Test Author'
+
+    url = 'https://www.youtube.com/watch?v=6GQRnPlephU'
+    api_key = 'test_api_key'
+
+    summarize(url, api_key)
+
+    mock_yt_transcripts.assert_called_once()
+    mock_openai_create.assert_called()
+    mock_youtube.assert_called_once_with(url)
+    assert os.path.exists(filename)
+    with open(filename, 'r') as f:
+        summary = f.read()
+        assert 'Transcript of' in summary
+        assert 'Executive Summary' in summary
+        assert 'Main Takeaways' in summary
+
+    os.remove(filename)
